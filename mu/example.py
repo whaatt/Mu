84b31e12d8c85572479f8ea9e406120b261307b3
Sanjay Kannan <whaatt@gmail.com>
2012-08-03 13:44:28 -0400

    Add some more examples.

--- mu/example.py
+++ mu/example.py
@@ -1,11 +1,14 @@
 from mu import mu as f
 
-factorial = f('return a*t(a-1) if a>1 else 1')
-fibonacci = f('return t(a-1)+t(a-2) if a>2 else 1')
-comprehend = f('return [n+a for n in range(1,13)]')
-example = f('salary = a*a; return salary')
+factorial = f('return a*t(a-1) if a>1 else 1') #Factorial Function
+fibonacci = f('return t(a-1)+t(a-2) if a>2 else 1') #Fibonacci Function
+comprehend = f('return [n+a for n in range(1,13)]') #List Comprehension
+example = f('salary = a*a; return salary') #Defining Variables
 
-print(factorial(6))
-print(fibonacci(8)) 
-print(comprehend(5)) 
-print(example(4))
+print(factorial(6)) #Result: 720
+print(fibonacci(8)) #Result: 21
+print(comprehend(5)) #Result: [6...17]
+print(example(4)) #Result: 16
+
+print(f('return a*3')(4)) #Result: 12
+print(f('cube = a**3; return cube')(4)) #Result: 64
