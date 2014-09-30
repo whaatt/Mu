42a82e6af7165f4c75b6423da87377fa813f1e8f
Sanjay Kannan <whaatt@gmail.com>
2014-09-29 22:56:59 -0700

    Add copious documentation.

--- example.py
+++ example.py
@@ -1,15 +1,38 @@
-from mu import mu as f
+from mu import mu
 
-#Usage: from mu import mu --> all you need to get started. import it as something for brevity's sake
-#Usage: mu(function) --> where function is a string of Python delimited by ':' and ';' as shown below
-#Usage: mu(function, False) --> where function looks like regular Python inside ''' this ''', as shown below
-#Usage: the variable *a* refers to your passed parameter, and the variable *t* refers to the function itself (for recursion)
+#here is some brief documentation on how you can use Mu in your programs
+#mu(function, [inline = True], [recurseName = 'this'], [paramName = 'param'])
+    
+    #function specifies the anonymous function where it is a string of Python
+    #delimited by semicolons to specify new lines; colons specify an indent level
+    #as well as leading tabs at the beginning of a line [OR] is simply a triple
+    #quoted string indented like normal Python code [see below]
 
-#Known Quirk: the global keyword will not work as expected within the context of your larger program
-#Known Quirk: In ''' mode, escape characters in strings Must Have Two Backslashes, e.g. '\\t' instead of '\t'
-#Known Quirk: In string mode, semicolons in strings must be doubled up ';;' to work properly.
+    #inline specifies whether the input is specified in single or double quotes [True]
+    #OR if it is in the triple quoted Python style mentioned previously [False]
+    
+    #recurseName specifies a function name that refers to the anonymous function itself
+    #paramName specifies a variable name that refers to function input variable
 
-#Temporary, Anonymous Functions
+#quirk: the global keyword may not work as expected within your larger program
+#quirk: escape characters in triple quoted strings are two backslashes; e.g. \\n
+#quirk: in inline mode, semicolons escape an arbitrary number of subsequent semicolons
+
+#f is just a convenience wrapper for
+#using inline mode examples where
+#recursion is done using t() and
+#input is specified by variable a
+
+#g is similarly a convience wrapper
+#for multiline mode using the same
+#altered recursion and parameter names
+
+def f(function): return mu(function, True, recurseName = 't', paramName = 'a')
+def g(function): return mu(function, False, recurseName = 't', paramName = 'a')
+
+#temporary and anonymous functions
+#and then some more complicated examples
+
 print(f('return a*3')(4)) #Result: 12
 print(f('cube = a**3; return cube')(4)) #Result: 64
 
@@ -17,28 +40,29 @@ print(f('if a==1:; :return 1; elif a%2 == 0:; :return t(a/2); else:; :return t(3
 print(f('def hello(number):; :if number==3:; ::return 3; :else:; ::return 0; return hello(a)')(3)) #Result: 3
 print(f('def pi():; :return 3.1415926535897932384626433832795028841971693993751; return pi()')()) #Result: PI
 
-#Example of False Flag, Designed to Look Like Native Syntax
-#Functions inside ''' ''' must only be internally consistent for indentation, and first line need not be indented properly
+#example of multiline syntax designed to look native
+#tabbing needs to only be internally consistent and
+#it does not matter the indentation of the first line
 
-if True:
-	print(f('''
-		if a > 2:
-			return t(a - 1) + t(a - 2)
-		else:
-			return 1
-	''', False)(9)) #Result: 34
-	
-#Example of using this with filter/map, compare with traditional lambda
+#fibonacci
+print(g('''
+    if a > 2:
+        return t(a - 1) + t(a - 2)
+    else:
+        return 1
+''')(9)) #Result: 34
+    
+#example of using this with filter or map
+#compare with limits of traditional lambda
 foo = [2, 18, 9, 22, 17, 24, 8, 12, 27]
 
-print ([i for i in filter(f('''
-	for i in range(2, round(a**0.5) + 1): #check primality
-		if a%i == 0: return False
-	return True
-''', False), foo)]) #Result: [2, 17]
-	
-#Functions As Variables
+print ([i for i in filter(g('''
+    for i in range(2, round(a ** 0.5) + 1): #check primality
+        if a % i == 0: return False #comments are amazing
+    return True #and they are entirely valid with Mu
+'''), foo)]) #Result: [2, 17]
 
+#use Mu as a convenience tool to compactly assign named methods
 factorial = f('return a*t(a-1) if a>1 else 1') #Factorial Function
 fibonacci = f('return t(a-1)+t(a-2) if a>2 else 1') #Fibonacci Function
 comprehend = f('return [n+a for n in range(1,13)]') #List Comprehension
--- mu.py
+++ mu.py
@@ -1,98 +1,130 @@
-def mu(function, string=True): #string input is default
-	def anonymous(a=0): #anonymous function, with default parameter = 0
-		def t(a=0): #used for recursion, t is shorthand for "this"
-			return anonymous(a)
-		
-		try:
-			if string: #if string input flag is set
-				code = function.strip().strip(';:').split(';')
-				
-				codes = []
-				lines = []
-				posit = 0
-				
-				#Get Rid of Blank Lines (';;'), which are actually semicolons (';')
-				while posit < len(code):
-					if code[posit] == '' and len(codes) > 0:
-						new = codes[len(codes)-1] + ';' + code[posit+1]
-						codes.pop()
-						codes.append(new)
-						posit = posit + 2
-						
-					else:
-						codes.append(code[posit])
-						posit = posit + 1
-				
-				#Add Level of Indentation with ':' (see below)
-				for part in range(len(codes)):
-					codes[part] = ':' + codes[part].strip()
-					
-				#Add a wrapper function so we can directly call exec() on it
-				codes = ['def output(a,t):'] + codes + ['return output(a,t)']
-				
-				#Various processing tasks
-				for pos in range(len(codes)):
-					tabs = 0
-					updated = codes[pos]
-					
-					#deal with escape sequences in strings first
-					for seq in [['\\','\\\\'],['\n','\\n'],['\t','\\t']]:
-						updated = updated.replace(seq[0], seq[1])
-					
-					#convert ':' to tabs
-					for char in updated:
-						if char == ':':
-							tabs = tabs + 1
-						else:
-							break
-					
-					#put code blocks in the same exec() call
-					if tabs > 0:
-						updated = '\t'*tabs + updated[tabs:]
-						updated = lines[len(lines)-1] + '\n' + updated
-						lines.pop()
-					lines.append(updated)
-					
-			else: #Python triple quote string mode
-				#split by '\n' itself, rather than ';'
-				code = function.strip().split('\n')
-				
-				pos = 0
-				tabs = 0
-				
-				#Tab standardization
-				try:
-					while code[1][pos:pos+1] == '\t':
-						tabs += 1
-						pos += 1
-				except IndexError:
-					tabs = 2
-				
-				try:
-					exec(code[0].strip() + '\n' + '1==1')
-				except SyntaxError:
-					tabs -= 1
-				#End Tab standardization
-				
-				#Re-add tabs properly
-				code[0] = '\t'*tabs + code[0].lstrip('\t')
-				code = [line[tabs-1:] for line in code]
-				
-				#Add wrapper function (see above)
-				code = '\n'.join(code)
-				lines = ['def output(a,t):\n' + code] + ['return output(a,t)']
-				
-			for line in lines:			
-				#Deal with variable conflicts in recursion
-				if line[0:7] == 'return ':
-					locals()['temporary'] = locals()['a']
-					globals()['a'] = locals()['a']
-					out = eval(line[7:])
-					locals()['a'] = locals()['temporary']
-					return out
-					
-				exec(line)
-		except IndexError: #Deal with empty functions
-			print('ERROR: Function cannot be empty.')
-					
-	return anonymous #return the anonymous function
+#inline specifies that the input is given in a single or double quoted string
+#inline input flag is set by default and can be specified otherwise manually
+
+#recurseName specifies what function name should be used for recursion
+#the default recursion function name is this; can be specified manually
+
+def mu(function, inline = True, recurseName = 'this', paramName = 'param'):
+    #define our anonymous function
+    #a is the parameter passed into
+    #our anonymous function
+    
+    def anonymous(param = 0):
+        #handle recursive function name
+        def this(param = 0): return anonymous(param)
+        locals()[recurseName] = locals()['this']
+        del locals()['this'] #recurseName replaced
+        
+        locals()[paramName] = locals()['param']
+        del locals()['param'] #paramName replaced
+        
+        try: #handle potential index error
+            if inline : #if function not specified in triple quotes
+                code = function.strip().strip(';:').split(';')
+                
+                segs = []
+                lines = []
+                position = 0
+                
+                #blank lines are actually demarcated with two
+                #semicolons, which we define in the spec for Mu
+                #to be converted to single semicolons; in other
+                #words, semicolon is the escape character for an
+                #arbitrary number of semicolons
+                
+                #iterate through lines
+                while position < len(code):
+                    if code[position] == '' and len(segs) > 0:
+                        #pop the last segment and add semicolon and next line
+                        newLast = segs[len(segs) - 1] + ';' + code[position + 1]
+                        
+                        segs.pop()
+                        segs.append(new)
+                        
+                        #skip two lines for semicolon
+                        position = position + 2
+                        
+                    else:
+                        #append line to segments
+                        segs.append(code[position])
+                        position = position + 1
+                
+                #indent all segments [see next]
+                for part in range(len(segs)):
+                    segs[part] = ':' + segs[part].strip()
+                    
+                #add a wrapper function so we can directly call exec() on it
+                #and have it return a valid function without any more hacks
+                segs = ['def output(' + paramName + ', ' + recurseName + '):'] + segs
+                segs += ['return output(' + paramName + ', ' + recurseName + ')']
+                
+                #various indentation and escape tasks
+                for pos in range(len(segs)):
+                    tabs = 0
+                    newSeg = segs[pos]
+                    
+                    #convert escape sequences in strings that are not escapes
+                    for seq in [['\\', '\\\\'], ['\n', '\\n'], ['\t', '\\t']]:
+                        newSeg = newSeg.replace(seq[0], seq[1])
+                    
+                    #convert colons to tabs
+                    for char in newSeg:
+                        if char == ':': tabs = tabs + 1
+                        else: break
+                    
+                    #put code blocks in the same exec() call
+                    #lines contains blocks classified by tabs
+                    
+                    if tabs > 0:
+                        newSeg = '\t' * tabs + newSeg[tabs:]
+                        newSeg = lines[len(lines) - 1] + '\n' + newSeg
+                        lines.pop()
+                    
+                    #add processed segment
+                    lines.append(newSeg)
+                    
+            else: #triple quote string mode
+                #split by newline rather than semicolon
+                code = function.strip('\n').split('\n')
+                
+                #standardize our tabs because tabs only need to be
+                #internally consistent within the triple quotes and
+                #not with respect to the program as a whole
+                
+                while False not in [line[0:4] == '    ' or line[0] == '\t' for line in code]:
+                    if code[0][0] == '\t': code = [line[1:] for line in code]
+                    elif code[0][0:4] == '    ': code = [line[4:] for line in code]
+                
+                #add one tab back for wrapper function
+                code = ['\t' + line for line in code]
+                
+                #add newlines back
+                code = '\n'.join(code)
+                
+                #add a wrapper function so we can directly call exec() on it
+                #and have it return a valid function without any more hacks
+                lines = ['def output(' + paramName + ', ' + recurseName + '):\n' + code + '\n']
+                lines += ['return output(' + paramName + ', ' + recurseName + ')']
+                
+            for line in lines:  
+                if line[0:7] == 'return ':
+                    #deal with variable conflicts in recursion
+                    #save our function parameter variable
+                    #at any given level of recursion or depth
+                    
+                    locals()['_paramTemp'] = locals()['paramName']
+                    globals()['paramName'] = locals()['paramName']
+                    recursiveOutput = eval(line[7:])
+                    locals()['paramName'] = locals()['_paramTemp']
+                    
+                    #send output straight back up
+                    return recursiveOutput
+                
+                #the magic
+                exec(line)
+                
+        except IndexError: #deal with empty functions
+            raise SyntaxError('Function cannot be empty.')
+    
+    #return our trickery
+    return anonymous
